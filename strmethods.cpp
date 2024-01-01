d86a65695555585fc49ce74c222301950c828091
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-01-01 15:23:38 -0500

    Finished up the string methods portion

--- strmethods.cpp
+++ strmethods.cpp
@@ -8,7 +8,24 @@ int main() {
 
     // the str.length(); method method returns the length of the string
     // str.empty() returns a boolean value | Returns true if string is the empty string
-    if (name.length() > 12) {
+
+    //Testing string slicing
+    std::cout << name[0] << std::endl;
+    std::cout << name.at(0) <<std::endl;
+
+    // Insert something into a string
+    name.insert(0, "@");
+    std::cout << name << std::endl;
+
+    // Find returns the position of any char in a string: returns __ if position not found
+    std::cout << "Space is at index " << name.find(' ') << "\n";
+
+    // Erase can eliminate a slice of a string
+    name.erase(0, 4);
+    std::cout << name << std::endl;
+
+    /*
+        if (name.length() > 12) {
         printf("Your name can't be over 12 characters\n");
     }
     else if (name.empty()) {
@@ -26,7 +43,7 @@ int main() {
     // str.clear() replaces the variable with an empty string
     name.clear();
     printf("Your name is: %s", name.c_str());
-
+    */
 
     return 0;
 }
