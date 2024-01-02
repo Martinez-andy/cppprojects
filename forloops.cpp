ec470a59f85ded0006ce2f9474ad6521040f4c9f
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-01-02 11:26:22 -0500

    Finished for loops

--- forloops.cpp
+++ forloops.cpp
@@ -2,8 +2,29 @@
 #include <stdio.h>
 
 int main() {
+    std::string name;
+
+    do {
+        printf("What is your name? ");
+        std::getline(std::cin, name);
+    } while(name.empty());
+
+    for (int i = 0; i <= name.length(); i++) {
+        printf("%c", name[i]);
+        printf(" ");
+    }
+    return 0;
+}
+
+
+
+
+
+/*
+int main() {
     for (int i = 3; i >= 0; i--) {
         printf("We are on our iteration %i\n", i);
     }
     return 0;
 }
+*/
