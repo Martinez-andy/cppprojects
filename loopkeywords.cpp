15d30c2cf943013248bee7aa434167ca20bc2cba
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-01-03 23:07:36 -0500

    Learning about breaks and commits

---
+++ loopkeywords.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+    // break and continue keywords
+
+    for (int i = 0; i <= 20; i++) {
+        if (i != 13) {
+            printf("%i\n", i);  
+        }
+    }
+    return 0;
+}
