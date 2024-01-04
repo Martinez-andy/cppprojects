6426ac60f9c922d4c899abe86cfb7fa305ff2a40
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-01-03 23:08:48 -0500

    Implemented alg using continue

--- loopkeywords.cpp
+++ loopkeywords.cpp
@@ -5,9 +5,10 @@ int main() {
     // break and continue keywords
 
     for (int i = 0; i <= 20; i++) {
-        if (i != 13) {
-            printf("%i\n", i);  
+        if (i == 13) {
+            continue;
         }
+        printf("%i\n", i);
     }
     return 0;
 }
