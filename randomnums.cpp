1800eb85d99a6d35836e405973d54b8a199c9089
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-01-04 23:20:06 -0500

    Created a getRandom function

--- randomnums.cpp
+++ randomnums.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <ctime>
 
+int getRand(int range);
+
 int main () {
     // Learning about random numbers
+    int range;
+    printf("What is the upper bound for random range?\n");
+    std::cin >> range;
 
-    srand(time(NULL));
-
-    // Picking a random number between 1 and 6
-    int num = (rand() % 6) + 1;
-
+    int num = getRand(range);
+    
     printf("%i\n", num);
     return 0;
+}
+
+int getRand(int range) {
+    srand(time(NULL));
+    return ((rand() % range) + 1);
 }
