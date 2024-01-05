9007e87df1fe1f3b24b9baa2a9ccfadf8ec138dd
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-01-05 00:37:28 -0500

    Learned about foreach loops

---
+++ foreach.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // foreach loop: usually used for iterable data sets
+
+    int nums[] = {1, 2, 3, 4, 5, 1203, 19023912, 6};
+    // For each loop below
+    for (int num : nums) {
+        printf("%i\n", num);
+    }
+}
